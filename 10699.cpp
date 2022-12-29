6f290a5e959f0269f8e2570bade889f30b991772
KSangmin <fhfpszmfnwm@naver.com>
2022-12-29 14:02:29 +0900

    [10699번] 오늘 날짜
    [10699번] 오늘 날짜

---
+++ 10699.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    time_t timer = time(NULL);
+    struct tm* t;
+    t = localtime(&timer);
+    cout << t->tm_year + 1900 << "-" << t->tm_mon + 1 << "-" << t->tm_mday;
+
+    return 0;
+}
