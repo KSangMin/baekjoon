8d09bc61718ac546e3227a925c9f440799edbac5
KSangMin <fhfpszmfnwm@naver.com>
2025-03-17 22:14:20 +0900

    [15680] 연세대학교

---
+++ 15680.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	string ans = (n == 0) ? "YONSEI" : "Leading the Way to the Future";
+	
+	cout << ans;
+
+	return 0;
+}
